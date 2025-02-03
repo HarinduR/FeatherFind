1259906e869775b54b55f747fa744efcc05ab5df
Harindu Raveen <harinduraveen9@gmail.com>
2025-02-03 14:19:46 +0530

    RASA install and project structure

--- Chatbot/chatbot_env/Scripts/activate.bat
+++ Chatbot/chatbot_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\IIT Lecs & Tutorials\Y1 SEM1\DSGP\Information genaration\FeatherFind\Chatbot\featherfind_env
+set VIRTUAL_ENV=E:\IIT Lecs & Tutorials\Y1 SEM1\DSGP\Information genaration\FeatherFind\Chatbot\chatbot_env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(featherfind_env) %PROMPT%
+set PROMPT=(chatbot_env) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(featherfind_env) 
+set VIRTUAL_ENV_PROMPT=(chatbot_env) 
 
 :END
 if defined _OLD_CODEPAGE (
